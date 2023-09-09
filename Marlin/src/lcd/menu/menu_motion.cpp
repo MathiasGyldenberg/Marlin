a144d9202b141ac4d30c61a09b8c9ee9e7afdbb0
MathiasGyldenberg <76220576+MathiasGyldenberg@users.noreply.github.com>
2023-09-09 20:50:59 +0200

    Move Assisted Tramming to menu_bed_leveling

--- Marlin/src/lcd/menu/menu_motion.cpp
+++ Marlin/src/lcd/menu/menu_motion.cpp
@@ -516,7 +516,7 @@ void menu_motion() {
   //
   // Assisted Bed Tramming
   //
-  #if ENABLED(ASSISTED_TRAMMING_WIZARD)
+  #if ENABLED(ASSISTED_TRAMMING_WIZARD) && DISABLED(LCD_BED_LEVELING)
     SUBMENU(MSG_TRAMMING_WIZARD, goto_tramming_wizard);
   #endif
 
